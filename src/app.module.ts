5bfd1985805b1d30c86076bf60eb7acc7075b8ad
Christian Oliveira <christian.soliveira@outlook.com>
2023-05-31 09:35:47 -0300

    feat(env): Importing and use NestJS config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+import { ConfigModule } from '@nestjs/config';
+
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -6,6 +8,7 @@ import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    ConfigModule,
     AuthModule,
     UserModule,
     BookmarkModule,
