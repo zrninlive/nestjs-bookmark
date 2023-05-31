c21414ce7a00f0deefb11374084f69d2ff4c16cc
Christian Oliveira <christian.soliveira@outlook.com>
2023-05-31 08:29:35 -0300

    feat(app): create project and auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,27 @@
+import * as argon from 'argon2';
+
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    return user;
+  }
+
+  signin() {
+    return {};
+  }
+}
